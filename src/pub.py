ecfdd895669037436ccf27840b3a4f5ec6b33d12
Andrew Wise <andrewswise2795@gmail.com>
2022-06-29 10:40:48 +0100

    first commit

---
+++ src/pub.py
@@ -0,0 +1,7 @@
+class Pub:
+    def __init__(self, _name, _till):
+        self.name = _name
+        self.till = _till
+        
+
+        
---
+++ tests/pub_tests.py
@@ -0,0 +1,13 @@
+from unicodedata import name
+import unittest
+from src.pub import Pub
+
+class TestPub(unittest.TestCase):
+    
+    def setUp(self):
+        self.pub = Pub("The Prancing Pony", 100.00)
+        self.pub1 = Pub("Bannermans", 200.00)
+
+    def test_pub_has_name(self):
+        self.assertEqual("The Prancing Pony", self.pub.name)
+    
